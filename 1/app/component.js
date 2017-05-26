a29b4ce0f8e69d55386365044dbe246f77fc5ca0
kay <1457675813@qq.com>
2017-05-26 12:18:20 +0800

    webpack

---
+++ 1/app/component.js
@@ -0,0 +1,5 @@
+export default(text="kay hello！")=>{
+	const element =document.createElement('p');
+	element.innerHTML = text;
+	return element;
+}
---
+++ 1/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PATHS = {
+	app:path.join(__dirname,'app'),
+	bulid:path.join(__dirname,'build'),
+};
+
+module.exports={
+	 entry:{
+	 	app:PATHS.app,
+	 },
+	    output: {
+	        path: PATHS.build,
+	        filename: '[name].js'
+	    },
+	    plugins: [
+	        new HtmlWebpackPlugin({
+	        	title:'Webpack demo',
+	        }),
+	    ],
+};
